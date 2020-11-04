f44b0c3b0eb69ff4b4f6d60cc2f3fecdf771e746
nadal12 <nadal12@hotmail.com>
2020-11-04 16:21:27 +0100

    Creació del projecte i afegirt codi @Andreulopess

---
+++ src/Elevator.java
@@ -0,0 +1,101 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Elevator {
+    private static final String DOWN = "cap avall";
+    private static final String UP = "cap adalt";
+    private final Random rand = new Random();
+    private int floor = rand.nextInt(10) + 1;
+    ArrayList <Integer> plantes = new ArrayList<>();
+
+    public void simulateElevator() throws IOException {
+        while (true){
+            System.out.println("Ascensor a planta " + floor);
+            System.out.print("Quina planta vols anar? ");
+
+            //Llegim entrada
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String  lines = br.readLine();
+
+            String[] strs = lines.trim().split("\\s+");
+
+            for (int i = 0; i < strs.length; i++) {
+                plantes.add( Integer.parseInt(strs[i]));
+            }
+
+            int destinationFloor = plantes.get(0);
+
+
+            int movement = getMovementDirection(destinationFloor);
+            if (movement == 0) {
+                System.out.println("Obrint portes");
+            } else {
+                moveElevator(destinationFloor, movement);
+            }
+        }
+    }
+
+
+
+    /* determine if movement needed is to up or to down */
+    private int getMovementDirection(int destinationFloor) {
+        int dif = destinationFloor - floor;
+        if (dif == 0) {
+            return 0;
+        }
+        return dif / Math.abs(dif);
+    }
+
+
+    private String getMovementName(int movement) {
+        return movement < 0 ? DOWN : UP;
+    }
+
+    private void moveElevator(int destination, int moveDirection) {
+        String movementName = getMovementName(moveDirection);
+        System.out.println("Ascensor l'ascensor esta anant ".concat(movementName).concat("..."));
+        comprovarDisPonibles(moveDirection);
+        while (floor != destination) {
+            floor += moveDirection;
+
+
+
+            System.out.println(floor);
+        }
+        System.out.println("Ascensor ha arribat");
+        plantes.remove(0);
+        System.out.println(plantes);
+    }
+
+    private void comprovarDisPonibles( int moveDirection){
+        if (moveDirection > 0) {
+            for (int i = 0; i < plantes.size(); i++) {
+                if (floor < plantes.get(i)) {
+                    System.out.println("Puc anar a planta: "
+                            +plantes.get(i));
+                }
+
+            }
+
+        }else{
+            for (int i = 0; i < plantes.size(); i++) {
+                if (floor > plantes.get(i)) {
+                    System.out.println("Puc anar a planta: "
+                            +plantes.get(i));
+                }
+
+            }
+
+        }
+
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Elevator().simulateElevator();
+    }
+}
