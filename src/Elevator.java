4573a8019130aedc5f29cb7a03c8ee9eefbcfc14
andreulopess <andreu.lopez.cortes@gmail.com>
2020-11-06 19:27:50 +0100

    Mmenut a que esperes a fer s ainterfisie aquest ascensort va tot volat

--- src/Elevator.java
+++ src/Elevator.java
@@ -1,3 +1,4 @@
+import javax.xml.transform.Source;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -10,7 +11,8 @@ public class Elevator {
     private static final String UP = "cap adalt";
     private final Random rand = new Random();
     private int floor = rand.nextInt(10) + 1;
-    ArrayList <Integer> plantes = new ArrayList<>();
+    private int movement;
+    ArrayList <Integer> entrada = new ArrayList<>();
     //De objecte planta
     ArrayList <Planta> Plantes = new ArrayList<>();
 
@@ -20,7 +22,7 @@ public class Elevator {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String  line = br.readLine();
         iniciarPlantes(line);
-        System.out.println(Plantes);
+        //System.out.println(Plantes);
 
         while (true){
             System.out.println("Ascensor a planta " + floor);
@@ -31,14 +33,25 @@ public class Elevator {
 
             String[] strs = lines.trim().split("\\s+");
 
+            //Aqui tenim sa llista de ses plantes pendents
             for (int i = 0; i < strs.length; i++) {
-                plantes.add( Integer.parseInt(strs[i]));
+                entrada.add( Integer.parseInt(strs[i]));
             }
+            //Posar plantes a pendets de visitar
+            for (int i = 0; i < entrada.size(); i++){
+                for (int j = 0; j < Plantes.size(); j ++){
+                    if (entrada.get(i) == Plantes.get(j).getNumPlanta()){
 
-            int destinationFloor = plantes.get(0);
+                        //System.out.println(entrada.get(i) +"  Es igual a "+Plantes.get(j).getNumPlanta());
+                        Plantes.get(j).setPendent(true);
+                    }
+                }
+            }
+            //Agafam desti es primer da sa llista d'entrades.
+            int destinationFloor = entrada.get(0);
 
+             movement = getMovementDirection(destinationFloor);
 
-            int movement = getMovementDirection(destinationFloor);
             if (movement == 0) {
                 System.out.println("Obrint portes");
             } else {
@@ -46,20 +59,20 @@ public class Elevator {
             }
         }
     }
+    void imprimirPendents (){
+        for (int i = 0; i < Plantes.size(); i++){
+            System.out.println("Planta: "+Plantes.get(i).getNumPlanta()+ " , pendent = "+Plantes.get(i).isPendent());
+        }
+
+    }
     void iniciarPlantes (String numPlantesString){
         int numplantes = Integer.parseInt(numPlantesString);
        Planta plantaMomentania ;
         for (int i = 0; i < numplantes+1; i++) {
             plantaMomentania = new Planta(i,false,false,false);
             Plantes.add(i,plantaMomentania);
-
         }
-
-
     }
-
-
-
     /* determine if movement needed is to up or to down */
     private int getMovementDirection(int destinationFloor) {
         int dif = destinationFloor - floor;
@@ -69,7 +82,6 @@ public class Elevator {
         return dif / Math.abs(dif);
     }
 
-
     private String getMovementName(int movement) {
         return movement < 0 ? DOWN : UP;
     }
@@ -77,42 +89,63 @@ public class Elevator {
     private void moveElevator(int destination, int moveDirection) {
         String movementName = getMovementName(moveDirection);
         System.out.println("Ascensor l'ascensor esta anant ".concat(movementName).concat("..."));
-        comprovarDisPonibles(moveDirection);
+        //comprovarDisPonibles(moveDirection);
         while (floor != destination) {
             floor += moveDirection;
-
-
-
+            plantaPendent();
             System.out.println(floor);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
         System.out.println("Ascensor ha arribat");
-        plantes.remove(0);
-        System.out.println(plantes);
+        System.out.println(entrada);
+    }
+
+    /*
+    * En aquest metode es comprova durant el moviment de lascensor si les plantes per les
+    * que passa estan a pendents. Si es aixi, s'atura.
+    * FALTA PER COMPROVAR si es boto es per pujar o baixar. Se pot emplear sa variable movement
+    * */
+    void plantaPendent(){
+        for (int j = 0; j < Plantes.size(); j ++){
+            if (floor == Plantes.get(j).getNumPlanta() &&  Plantes.get(j).isPendent() ){
+               //Llevam pendent perque ja lhem visitada
+                Plantes.get(j).setPendent(false);
+                //Levar planta de arraylist
+                for (int i = 0; i < entrada.size();i++){
+                    if (entrada.get(i) == floor){
+                        entrada.remove(i);}
+                }
+                System.out.println("Obrint portes de la planta: "+Plantes.get(j).getNumPlanta());
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Tancant portes");
+            }
+        }
     }
 
     private void comprovarDisPonibles( int moveDirection){
         if (moveDirection > 0) {
-            for (int i = 0; i < plantes.size(); i++) {
-                if (floor < plantes.get(i)) {
+            for (int i = 0; i < entrada.size(); i++) {
+                if (floor < entrada.get(i)) {
                     System.out.println("Puc anar a planta: "
-                            +plantes.get(i));
+                            +entrada.get(i));
                 }
-
             }
-
         }else{
-            for (int i = 0; i < plantes.size(); i++) {
-                if (floor > plantes.get(i)) {
+            for (int i = 0; i < entrada.size(); i++) {
+                if (floor > entrada.get(i)) {
                     System.out.println("Puc anar a planta: "
-                            +plantes.get(i));
+                            +entrada.get(i));
                 }
-
             }
-
         }
-
-
-
     }
 
     public static void main(String[] args) throws IOException {
--- src/Planta.java
+++ src/Planta.java
@@ -2,6 +2,7 @@ public class Planta {
 
     private boolean pujar;
     private boolean baixar;
+    private boolean pendent;
     private int numPlanta;
     private boolean ascensorAturat;
 
@@ -10,6 +11,19 @@ public class Planta {
         this.baixar = baixar;
         this.numPlanta = numPlanta;
         this.ascensorAturat = ascensorAturat;
+    }
+    void obri_portes() throws InterruptedException {
+        Thread.sleep(500);
+        this.ascensorAturat = true;
+
+    }
+
+    public boolean isPendent() {
+        return pendent;
+    }
+
+    public void setPendent(boolean pendent) {
+        this.pendent = pendent;
     }
 
     public boolean isPujar() {
